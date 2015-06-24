d35d002e490ec66e254e4c54c9ae7bd3c06e0453
Nesiehr <rheisen@live.com>
2015-06-24 18:01:57 -0400

    Massive Update

---
+++ file/script/canvas-script.js
@@ -0,0 +1,4 @@
+$('.row .box-header').flowtype({fontRatio :25});
+$('.row .text-area').flowtype({fontRatio :25});
+$('.column .box-header').flowtype({fontRatio :63});
+$('.column .text-area').flowtype({fontRatio :66});
