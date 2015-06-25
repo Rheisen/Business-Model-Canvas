49b8b6312acdd632d2656b425e423cc250d21ae2
Nesiehr <rheisen@live.com>
2015-06-25 10:33:25 -0400

    Mobile Update

--- file/script/canvas-script.js
+++ file/script/canvas-script.js
@@ -1,4 +1,2 @@
-$('.row .box-header').flowtype({fontRatio :25});
-$('.row .text-area').flowtype({fontRatio :25});
-$('.column .box-header').flowtype({fontRatio :63});
-$('.column .text-area').flowtype({fontRatio :66});
+$('.box-header h2').flowtype({fontRatio :15});
+$('.box-header i').flowtype({fontRatio :1});
